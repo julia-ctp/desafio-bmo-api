259ec79644d9209c033598f3f201fa80ff2c6c11
julia-ctp <juliactp.santos@gmail.com>
2025-12-08 07:07:22 -0300

    docs: add swagger documentation for /notices endpoints

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,12 @@
 import express from "express";
-import swaggerUi from "swagger-ui-express";
-import swaggerDocument from "./docs/swagger/swagger.json";
 import { noticesRouter } from "./modules/notices/notices.routes";
 import { errorHandler } from "./middlewares/errorHandler";
+import { setupSwagger } from "./docs/swagger/swagger";
 
 const app = express();
 app.use(express.json());
 
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+setupSwagger(app)
 app.use(noticesRouter);
 app.use(errorHandler);
 

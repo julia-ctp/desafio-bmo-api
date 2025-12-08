a5980684aec8c8ed46ad7c56b353b09df3c153e3
julia-ctp <juliactp.santos@gmail.com>
2025-12-08 07:07:22 -0300

    docs: add swagger documentation for /notices endpoints

---
+++ src/docs/swagger/schemas/notice.schemas.ts
@@ -0,0 +1,48 @@
+const noticeEnumSchema = {
+  NoticeType: {
+    type: "string",
+    enum: ["importante", "informativo", "nenhum"],
+  },
+};
+
+const noticeMainSchemas = {
+  Notice: {
+    type: "object",
+    properties: {
+      id: { type: "string" },
+      employeeId: { type: "string" },
+      type: {
+        $ref: "#/components/schemas/NoticeType",
+      },
+      content: { type: "string" },
+      createdAt: { type: "string", format: "date-time" },
+      updatedAt: { type: "string", format: "date-time" },
+    },
+  },
+
+  CreateNoticeInput: {
+    type: "object",
+    required: ["type", "content"],
+    properties: {
+      type: {
+        $ref: "#/components/schemas/NoticeType",
+      },
+      content: { type: "string" },
+    },
+  },
+
+  UpdateNoticeInput: {
+    type: "object",
+    properties: {
+      type: {
+        $ref: "#/components/schemas/NoticeType",
+      },
+      content: { type: "string" },
+    },
+  },
+};
+
+export default {
+  ...noticeEnumSchema,
+  ...noticeMainSchemas,
+};
---
+++ src/docs/swagger/swagger.ts
@@ -0,0 +1,34 @@
+import swaggerUi from "swagger-ui-express";
+import { Express } from "express";
+import noticeSchemas from "./schemas/notice.schemas";
+import noticesPaths from "./paths/notices.paths";
+
+const swaggerDocument = {
+  openapi: "3.0.0",
+  info: {
+    title: "Desafio BMO",
+    version: "1.0.0",
+    description: "API de Avisos Internos",
+  },
+  servers: [{ url: "http://localhost:3000", description: "Servidor Local" }],
+  paths: {
+    ...noticesPaths
+  },
+  components: {
+    schemas: {
+      ...noticeSchemas
+    },
+  },
+  securitySchemes: {
+    bearerAuth: {
+      type: "http",
+      scheme: "bearer",
+      bearerFormat: "JWT",
+    },
+  },
+  security: [{ bearerAuth: [] }],
+};
+
+export function setupSwagger(app: Express) {
+  app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+}

1e8fed1bb898dce043672367ba3be4d7faca748f
julia-ctp <juliactp.santos@gmail.com>
2025-12-10 00:56:48 -0300

    feat: cors configuration

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { noticesRouter } from "./modules/notices/notices.routes";
 import { errorHandler } from "./middlewares/errorHandler.middleware";
 import { setupSwagger } from "./docs/swagger/swagger";
@@ -8,10 +9,19 @@ import { authRouter } from "./modules/auth/auth.routes";
 const app = express();
 app.use(express.json());
 
-app.use(authRouter)
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:3001"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
+app.use(authRouter);
 app.use(noticesRouter);
-app.use(contactRouter)
-setupSwagger(app)
+app.use(contactRouter);
+setupSwagger(app);
 app.use(errorHandler);
 
 export default app;

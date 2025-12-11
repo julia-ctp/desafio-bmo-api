b9ec4c1273ff617aa04ac24368843cd45002dfcb
Julia Catapan <151873864+julia-ctp@users.noreply.github.com>
2025-12-10 23:48:34 -0300

    Revert "refactor: adjust contact table"
    This reverts commit 5c6d5bc88633c1cc5c09c9eb7107a5900888b206.

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,4 @@
-import "dotenv/config";
 import express from "express";
-import cors from "cors";
 import { noticesRouter } from "./modules/notices/notices.routes";
 import { errorHandler } from "./middlewares/errorHandler.middleware";
 import { setupSwagger } from "./docs/swagger/swagger";
@@ -8,19 +6,9 @@ import { contactRouter } from "./modules/contact/contact.route";
 import { authRouter } from "./modules/auth/auth.routes";
 const app = express();
 app.use(express.json());
-app.use(cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-}));
 app.use(authRouter);
 app.use(noticesRouter);
 app.use(contactRouter);
 setupSwagger(app);
 app.use(errorHandler);
 export default app;
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
--- dist/docs/swagger/swagger.js
+++ dist/docs/swagger/swagger.js
@@ -1,10 +1,6 @@
 import swaggerUi from "swagger-ui-express";
 import noticeSchemas from "./schemas/notice.schemas";
 import noticesPaths from "./paths/notices.paths";
-import contactSchemas from "./schemas/contact.schemas";
-import contactPaths from "./paths/contact.paths";
-import authSchemas from "./schemas/auth.schemas";
-import authPaths from "./paths/auth.paths";
 const swaggerDocument = {
     openapi: "3.0.0",
     info: {
@@ -14,15 +10,11 @@ const swaggerDocument = {
     },
     servers: [{ url: "http://localhost:3000", description: "Servidor Local" }],
     paths: {
-        ...noticesPaths,
-        ...contactPaths,
-        ...authPaths,
+        ...noticesPaths
     },
     components: {
         schemas: {
-            ...noticeSchemas,
-            ...contactSchemas,
-            ...authSchemas,
+            ...noticeSchemas
         },
     },
     securitySchemes: {
